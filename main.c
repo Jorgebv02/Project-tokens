4327b4a5108e9dfaa6b605d6057de063ce28075e
Jorge Barquero Villalobos <jorgebv02@gmail.com>
2018-03-12 17:02:45 -0600

    Version 1.1
    # Ya se ve el tablero.
    # Inicio de la manipulación de archivos.

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+**************************************************
+Código fuente: fichas.c
+Versión: 1.0
+Fecha de creación: 7 de marzo del 2018.
+Última actualización: N/A.
+Autores: Jorge Barquero Villalobos,
+         Juan Diego Escobar Sáncehz,
+         Yericka Lafuente Ovares,
+         basados en la Prof. Ericka Solano Fernández.
+Descripción: código que permite la manipulación de fichas
+en un tablero y realizar acciones sobre el mismo.
+Notas:
+
+Construido con: Gcc 4.9.
+**************************************************
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -40,19 +58,78 @@ void llenarTablero(matriz tablero, int n){
     int i, j;
     for(i = 0; i < n; i++){
         for(j = 0; j < n; j++){
-            tablero[i][j] = 0;
+            tablero[i][j] = 0;  // Puesto que todavía no hay fichas sobre él.
         }
     }
 }
 
 // Muestra en pantalla el tablero creado.
 void mostrarTablero(matriz tablero, int n){
-    int i, j;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            printf("%i ", tablero[i][j]);
+    /*
+    Aquí, el tablero se despliega de la siguiente manera:
+
+       1   2   3   4   5...
+    1|___|___|___|___|_F_|
+    2
+    3       # Si una ficha se encuentra dentro
+    4         de una posición de la matriz: representarla
+    5         mediante algún símbolo.
+    .
+    .
+    .
+    */
+
+    // Contador para poder imprimir el tablero.
+    int contador = n;
+    // 3 contadores para poder iterar sobre la matriz.
+    int borde, contenido, auxiliar;
+
+    // Primer ciclo para imprimir los números del "techo".
+    for (borde = 1; borde <= contador; borde++){
+        // Para poder imprimir a la izquierda los números también.
+        if (borde == 1){
+            printf("     %d   ", borde);
+        } else if(borde >= 10){         // Si no, los números se empiezan a ver raro.
+            printf("%d   ", borde);
+
+        } else{
+            printf(" %d   ", borde);
+        }
+    }
+
+    // Salto de línea.
+    printf("\n");
+
+    // Ahora en este for se imprime el contenido del tablero.
+    for (contenido = 0; contenido < n; contenido++){
+        // Los primeros 9 dígitos hay que imprimirlos de otra manera.
+        if (contenido < 9){
+                printf("  %d", (contenido + 1));
+        }else{
+                printf(" %d", (contenido + 1));
+        }
+
+        // Ya esto sería todo el contenido del tablero.
+        for (auxiliar = 1; auxiliar <= n; auxiliar++){
+            printf("|_");
+            if (tablero[contenido][auxiliar] == 1){
+                printf("F_|");
+            }
+            else{
+                printf("__|");
+            }
         }
         printf("\n");
+    }
+}
+
+// Método que indica en cual fila y columna poner una ficha.
+void agregarFicha(matriz tablero, int n, int fila, int columna){
+    if ((fila >= n) || (columna >= n)){
+        printf("Esa posición no existe en el tablero.");
+    }
+    else{
+        printf("Ak7.");
     }
 }
 
@@ -61,6 +138,7 @@ int main()
     int tamTablero = 5;
     matriz tablero = generarTablero(tamTablero);
     llenarTablero(tablero, tamTablero);
+
     mostrarTablero(tablero, tamTablero);
     return 0;
 }
